c4625acd9806030d9889a64929bae4b1d1325070
licandragon <zekrom06@gmail.com>
2023-10-16 20:42:04 -0700

    Se crea archivo del que se va a leer los querys, se importa 'fs' para el manejo de archivos y se abre el fichero

--- lexer_sql.js
+++ lexer_sql.js
@@ -1,7 +1,28 @@
-const reserdas_condicionales = ["WHERE", "ORDER BY", "LIMIT","LIKE","BETWEEN"];
+const reserdas_condicionales = [
+  "WHERE",
+  "ORDER BY",
+  "LIMIT",
+  "LIKE",
+  "BETWEEN",
+];
 const reservadas_order = ["ASC", "DESC"];
-const condicionales = ["<",">",">=","<=","=","NOT","OR","AND"];
-const operador = ["*", ]
-const reservadas_iniciales = ["SELECT", "INSERT","CREATE", "UPDATE", "DELETE","ALTER","USE"];
-const reservadas_secuenciales = ["FROM","SET"]
+const condicionales = ["<", ">", ">=", "<=", "=", "NOT", "OR", "AND"];
+const operador = ["*"];
+const reservadas_iniciales = [
+  "SELECT",
+  "INSERT",
+  "CREATE",
+  "UPDATE",
+  "DELETE",
+  "ALTER",
+  "USE",
+];
+const reservadas_secuenciales = ["FROM", "SET"];
 
+fs.readFile("querys.txt", "utf8", (err, data) => {
+  var cadena_split = data.split("\n");
+  cadena_split = cadena_split.map(element => {
+    return element.trim();
+  });
+  
+});
