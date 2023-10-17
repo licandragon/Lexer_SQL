43c6dc6cccc91664de67d944273a6a10ec10ac38
licandragon <zekrom06@gmail.com>
2023-10-16 20:14:03 -0700

    Declaracion de Palabras Reservadas, Operadores y Condicionales que mas adelante seran utilizadas

---
+++ lexer_sql.js
@@ -0,0 +1,7 @@
+const reserdas_condicionales = ["WHERE", "ORDER BY", "LIMIT","LIKE","BETWEEN"];
+const reservadas_order = ["ASC", "DESC"];
+const condicionales = ["<",">",">=","<=","=","NOT","OR","AND"];
+const operador = ["*", ]
+const reservadas_iniciales = ["SELECT", "INSERT","CREATE", "UPDATE", "DELETE","ALTER","USE"];
+const reservadas_secuenciales = ["FROM","SET"]
+
