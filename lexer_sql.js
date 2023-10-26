70335806087c137d5824531e12b908f293d828dd
licandragon <zekrom06@gmail.com>
2023-10-25 22:10:57 -0700

    se crean arreglos de constantes para comillas y carteres especiales, se reducen las condicionales

--- lexer_sql.js
+++ lexer_sql.js
@@ -1,5 +1,7 @@
-const fs = require('fs');
-const reserdas_condicionales = [
+const fs = require("fs");
+const tokens = [];
+
+/*const reservadas_condicionales = [
   "WHERE",
   "ORDER BY",
   "LIMIT",
@@ -18,8 +20,31 @@ const reservadas_iniciales = [
   "ALTER",
   "USE",
 ];
+const reservadas_secuenciales = ["FROM", "SET"];*/
+const comillas = ["'", '"', "`"];
 
-const reservadas_secuenciales = ["FROM", "SET"];
+const caracteres_especiales = [
+  "*",
+  "+",
+  "-",
+  "/",
+  "%",
+  "=",
+  "<",
+  ">",
+  "{",
+  "}",
+  "[",
+  "]",
+  "(",
+  ")",
+  "&",
+  "|",
+  "^",
+  ";",
+  ",",
+  ":",
+];
 
 function descomponerQuery(query) {
   const componentes = [];
@@ -27,51 +52,38 @@ function descomponerQuery(query) {
   let en_comillas = false;
 
   for (let i = 0; i < query.length; i++) {
-      const caracter = query[i];
+    const caracter = query[i];
 
-      if (caracter === "'" || caracter === "\"" || caracter === "\`") {
-          // Cambiamos el estado de en_comillas cuando encontramos comillas
-          en_comillas = !en_comillas;
-          componente += caracter;
-      } else if (caracter === " " && !en_comillas) {
-          // Si encontramos un espacio fuera de las comillas, guardamos el componente actual y lo reiniciamos
-          if (componente.trim() !== "") {
-              componentes.push(componente.trim());
-          }
-          componente = "";
-      } else if (caracter === "=" || caracter === "<" || caracter === ">") {
-          // Si encontramos un operador de comparación, lo tratamos como un componente separado
-          if (componente.trim() !== "") {
-              componentes.push(componente.trim());
-          }
-         // Verificamos si estamos en el último carácter antes de acceder al siguiente
-            if (i < query.length - 1) {
-                if ((caracter === "<" || caracter === "<" )&& query[i + 1] === "=") {
-                  console.log("Si");
-                    componente = caracter + query[i + 1];
-                    i++; // Avanzamos un carácter adicional
-                } else {
-                    componente = caracter;
-                }
-            } else {
-                componente = caracter;
-            }
-      } else if (caracter === ";") {
-          // Si encontramos un punto y coma, lo tratamos como un componente separado
-          if (componente.trim() !== "") {
-              componentes.push(componente.trim());
-          }
-          componentes.push(caracter);
-          componente = "";
-      } else {
-          // Agregamos el carácter al componente actual
-          componente += caracter;
+    if (comillas.includes(caracter)) {
+      // Cambiamos el estado de en_comillas cuando encontramos comillas
+      en_comillas = !en_comillas;
+      if (componente.trim() !== "") {
+        componentes.push(componente.trim());
       }
+      componentes.push(caracter);
+      componente = "";
+    } else if (caracter === " " && !en_comillas) {
+      // Si encontramos un espacio fuera de las comillas, guardamos el componente actual y lo reiniciamos
+      if (componente.trim() !== "") {
+        componentes.push(componente.trim());
+      }
+      componente = "";
+    } else if (caracteres_especiales.includes(caracter) && !en_comillas) {
+      // Si encontramos un operador de comparación, lo tratamos como un componente separado
+      if (componente.trim() !== "") {
+        componentes.push(componente.trim());
+      }
+      componentes.push(caracter);
+      componente = "";
+    } else {
+      // Agregamos el carácter al componente actual
+      componente += caracter;
+    }
   }
 
   // Agregamos el último componente si no está vacío
   if (componente.trim() !== "") {
-      componentes.push(componente.trim());
+    componentes.push(componente.trim());
   }
 
   return componentes;
@@ -81,16 +93,13 @@ fs.readFile("querys.txt", "utf8", (err, data) => {
   if (err) {
     console.error(err);
     return;
-  }   
+  }
   var query_split = data.split("\n");
-  query_split = query_split.map(element => {
+  query_split = query_split.map((element) => {
     return element.trim();
   });
-  
-  query_split.forEach(function(query){
+
+  query_split.forEach(function (query) {
     console.log(descomponerQuery(query));
   });
-
-  
-
 });
