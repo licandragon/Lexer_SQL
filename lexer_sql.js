4720834c30991b0d41917ed37b3a1f24b365ca50
licandragon <zekrom06@gmail.com>
2023-11-04 13:05:48 -0700

    se aplica la funcion .filter() para omitr los renglones vacios que pueda contener el archivo querys.txt

--- lexer_sql.js
+++ lexer_sql.js
@@ -53,16 +53,15 @@ function descomponerQuery(query) {
       }
       componentes.push(caracter);
       componente = "";
-    } else if(esNumero(caracter) && esNumero(componente)){
+    } else if (esNumero(caracter) && esNumero(componente)) {
       //si encontramos un numero y el componente tambien es un numero tambien lo agregamos
       componente += caracter;
-      if(query[i+1] === "." ){
+      if (query[i + 1] === ".") {
         //Si el siguiente caracter es un "." se añade como parte del componente (numero punto flotante)
-        componente += query[i+1];
+        componente += query[i + 1];
         i++;
       }
-    }
-    else {
+    } else {
       // Agregamos el carácter al componente actual
       componente += caracter;
     }
@@ -146,23 +145,23 @@ Promise.all([leer_tokens, leer_querys])
       }
     });
 
-    //Se procesa los querys para ser descompuestos en sus componentes
-    let query_split = querysData.split("\n");
+    //Se procesa los querys para ser descompuestos en sus componentes y se filtran los elementos vacios
+    let query_split = querysData.split("\n").filter((element) => element.trim().length > 0);
     query_split = query_split.map((element) => {
       return element.trim();
     });
-
+  
     query_split.forEach(function (query) {
       querys_descompuestos.push(descomponerQuery(query));
     });
     //console.log(tokens);
     // console.log(querys_descompuestos);
-   // console.log(tokenizar(tokens, querys_descompuestos[0]));
-    const tokenizados = []
+    // console.log(tokenizar(tokens, querys_descompuestos[0]));
+    const tokenizados = [];
     querys_descompuestos.forEach(function (query, indice) {
-      tokenizados.push(tokenizar(tokens,query));
+      tokenizados.push(tokenizar(tokens, query));
       console.log(
-        `Query ${indice+1}: ${query} \n Version Tokenizada: ${tokenizar(
+        `Query ${indice + 1}: ${query} \n Version Tokenizada: ${tokenizar(
           tokens,
           query
         )}`
